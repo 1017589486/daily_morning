c2bd9e034f0fa6c5842a9bdf8cc99ab8adc348c5
1017589486 <51186824+1017589486@users.noreply.github.com>
2022-08-22 01:17:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 def get_date():
-  return s% %today
+  today1= today.strftime("%Y-%m-%d %H:%M:%S")
+  return today1
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
