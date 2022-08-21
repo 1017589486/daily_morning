bab561a3f3c663bbfc17bebbe5de2dacaed13f90
1017589486 <51186824+1017589486@users.noreply.github.com>
2022-08-22 01:50:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return (next - today).days + 29
+  return (next - today).days + 28
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
